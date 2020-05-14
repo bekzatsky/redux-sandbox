bc4c87ad3386e43c1145753a7b92e53a07e92333
bekzatsky <mn.bekzat@gmail.com>
2020-05-14 10:28:04 +0600

    payload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
             return state + 1;
+        case 'RND':
+            return state + action.payload;
         case 'DEC':
             return state - 1;
         default:
@@ -15,6 +17,14 @@ const store = createStore(reducer);
 
 document.getElementById('inc').addEventListener('click', () => {
     store.dispatch({type: 'INC'});
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch({
+        type: 'RND',
+        payload
+    });
 });
 
 document.getElementById('dec').addEventListener('click', () => {
